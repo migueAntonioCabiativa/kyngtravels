057aa1229192680f287ffb136c5f7be55c59acb6
migueAntonioCabiativa <miancalo.lopez@gmail.com>
2022-08-03 22:52:17 +0000

    Se agregó configuracion de Email para enviar correos. agregando layouts y el objeto ESO

--- indexp.php
+++ indexp.php
@@ -3,8 +3,11 @@
   // ------ Documento logico  --------
   // -----------------------------------------------------------------------
   include("objects/DAO.php");$CON = new DAO();
+  //include("objects/ESO.php");$EMAIL = new ESO();
 
   $carruselInfo = $CON->getCarruselInfo();
+
+//  $EMAIL->Correo();
 
   $titulo =   $carruselInfo['1']['0'];
 
---
+++ objects/ESO.php
@@ -0,0 +1,55 @@
+<?php //------            ESO (Email Send Object) Objeto de envio de Correo               ------------
+
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+
+require ("layouts/src/Exception.php");
+require ("layouts/src/PHPMailer.php");
+require ("layouts/src/SMTP.php");
+
+
+class ESO{
+  private $email="keilys.naranjo@kyngtravels.com";
+  private $password="@1082846392Kn";
+  public function Correo(){
+    $mail = new PHPMailer(True);                //Instantiation and passing `true` enables exceptions
+
+    try {                  //Enable verbose debug output                  //Enable verbose debug output
+
+      $mail->isSMTP();                                            //Send using SMTP
+      $mail->Host = "ssl://smtp.hostinger.com";                   //Set the SMTP server to send through
+      $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+      $mail->Username   = $this->email;                           //SMTP username
+      $mail->Password   = $this->password;                               //SMTP password
+      $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+      $mail->Port       = 465;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+      //Recipients
+      $mail->setFrom($this->email, 'keilys');
+      $mail->addAddress('miancalo.lopez@gmail.com', 'Viaja con nosotros');     //Add a recipient
+      //$mail->addAddress('ronaldandresmh@gmail.com', 'Creador MOODIE');               //Name is optional
+      //$mail->addReplyTo('info@example.com', 'Information');
+      //$mail->addCC('cc@example.com');
+      //$mail->addBCC('bcc@example.com');
+
+      //Attachments
+      //$mail->addAttachment('reports/reporte.pdf');          //Add attachments
+      //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');  //Optional name
+
+      //Content
+      $mail->isHTML(true);                                  //Set email format to HTML
+      $mail->Subject = 'Reporte general del sistema MOODIE';
+      $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+      $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+      $mail->send();
+      //echo 'Message has been sent';
+    } catch (Exception $e) {
+      echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+      //return $promedio_dia;}
+  }
+}
+?>
