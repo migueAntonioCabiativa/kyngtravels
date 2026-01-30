692148277f7039fbc934df4cf24af81cf0e0b731
Miguel Cabiativa <mcabiativa@geprom.com>
2026-01-29 22:58:45 -0500

    arreglando prisma

--- src/app/lib/prisma.ts
+++ src/app/lib/prisma.ts
@@ -1,5 +1,5 @@
 import { PrismaMariaDb } from '@prisma/adapter-mariadb';
-import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaClient } from '../../generated/prisma/client';
 
 const adapter = new PrismaMariaDb({
   host: process.env.DATABASE_HOST,
